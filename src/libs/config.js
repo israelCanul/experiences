fe7310ccb1ecbdbf55bc706bdf88e01ee1c5bd34
israel <israel@cloudware.mx>
2021-10-05 15:45:30 -0500

    Retrieve de last endpoint api

--- src/libs/config.js
+++ src/libs/config.js
@@ -2,7 +2,7 @@ export const apiUrl = process.env.NODE_ENV !== "production" ? "" : "";
 export const apiUrlSF =
   process.env.NODE_ENV !== "production"
     ? "https://softwaresanchez.mx"
-    : "http://desarrollo.apisalesforce.com";
+    : "https://softwaresanchez.mx";
 export const apiWheater =
   process.env.NODE_ENV !== "production"
     ? "https://api.openweathermap.org/data/2.5/forecast?id=3531673&APPID=cb2558da57ff93c9bd45f6c9fbd8e6ea&units=imperial"
